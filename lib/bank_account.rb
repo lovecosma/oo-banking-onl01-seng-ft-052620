522e3d9083e5b3057cb1d3c405a4bae86a4c5e5a
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-12 21:52:55 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,7 +17,11 @@ def display_balance
 end
 
 def valid?
-return true if self.status == "open" && self.balance > 0
+if self.status == "open" && self.balance > 0
+  return true 
+else
+  return false
+end 
 end
 
 
