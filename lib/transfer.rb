804bda49421105f7dde1bc6088dc96e013322c16
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-12 21:57:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,13 @@
 class Transfer
   attr_accessor :sender, :receiver, :status, :amount
+  
 def initialize(sender, receiver, amount)
   @sender = sender
   @receiver = receiver
   @amount = amount
   @status = "pending"
 end
+
+
 
 end
