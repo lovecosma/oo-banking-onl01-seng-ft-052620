f7500828d8e008d3f881bdf18187a38c11ee61c6
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-12 21:45:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,16 @@
 class BankAccount
 attr_accessor :balance, :status
 attr_reader :name
+
+
+
 def initialize(name)
   @name = name
   @balance = 1000
   @status = "open"
 end
+
+
 
 def deposit(amount)
 @balance += amount
